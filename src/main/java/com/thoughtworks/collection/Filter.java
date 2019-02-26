d8335bf6a86765b2fddf6cbdcaf1b10b91d60c43
Song <songnan17@qq.com>
2019-02-26 11:46:15 +0800

    Complete function filterEven.

--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -2,19 +2,26 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class Filter {
 
-    List<Integer>  array;
+    List<Integer> array;
 
     public Filter(List<Integer> array) {
-     this.array = array;
+        this.array = array;
     }
 
     public List<Integer> filterEven() {
-        throw new NotImplementedException();
+        List<Integer> resultList = new ArrayList<Integer>();
+        for (Integer number : array) {
+            if (number % 2 == 0) {
+                resultList.add(number);
+            }
+        }
+        return resultList;
     }
 
     public List<Integer> filterMultipleOfThree() {
