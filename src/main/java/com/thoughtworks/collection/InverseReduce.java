61a0e4dadb14e177560a16f00ab6a347cf22eb88
Song <songnan17@qq.com>
2019-02-26 13:21:41 +0800

    Complete function divideToSmaller.

--- src/main/java/com/thoughtworks/collection/InverseReduce.java
+++ src/main/java/com/thoughtworks/collection/InverseReduce.java
@@ -2,6 +2,7 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -9,13 +10,18 @@ public class InverseReduce {
 
     private Random random = new Random();
 
-    public InverseReduce() {}
+    public InverseReduce() {
+    }
 
     public InverseReduce(Random random) {
         this.random = random;
     }
 
     public List<Integer> divideToSmaller(int number) {
-        throw new NotImplementedException();
+        List<Integer> resultList = new ArrayList<Integer>();
+        for (int i = number - random.nextInt(3); i >= 0; i -= random.nextInt(3)) {
+            resultList.add(i);
+        }
+        return resultList;
     }
 }
